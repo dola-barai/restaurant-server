49d979f500aff0f5055bd2e9f7205d716f0aed2c
Dola Barai <dola43dc@gmail.com>
2023-09-22 21:14:43 +0600

    save user data

--- index.js
+++ index.js
@@ -23,14 +23,40 @@ async function run() {
     try {
 
         const menuCollection = client.db('opulenza').collection('menu')
-
+        const userCollection = client.db('opulenza').collection('users')
 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result)
         })
 
-        await client.connect();
+        app.post('/menu', async (req, res) => {
+            const menu = req.body;
+            console.log(menu);
+            const result = await menuCollection.insertOne(menu);
+            res.send(result)
+        })
+
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result)
+          })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+      
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query)
+      
+            if (existingUser) {
+              return res.send({ message: 'user already exist' })
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+          })
+
+
+        // await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -43,7 +69,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Restaurant ongoing!')
 })
 
 app.listen(port, () => {
