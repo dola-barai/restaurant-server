fdcb87ee1b5ea84687cdf4196c122c9128ba340a
Dola Barai <dola43dc@gmail.com>
2023-09-26 00:15:35 +0600

    jwt added

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function run() {
             res.send(result)
         })
 
-        app.post('/users', verifyAdmin, async (req, res) => {
+        app.post('/users', async (req, res) => {
             const user = req.body;
 
             const query = { email: user.email }
