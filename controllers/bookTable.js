5ba2fda8f252f30f56f50f855d1b3b8ef27c39cd
Saurabh Kashyap <125493327+Sau0908@users.noreply.github.com>
2023-10-11 20:03:11 +0530

    Update bookTable.js

--- controllers/bookTable.js
+++ controllers/bookTable.js
@@ -59,7 +59,7 @@ export const updateBooking = async (req, res) => {
 };
 
 export const confirmingEmail = async (req, res) => {
-    const { reservationId, userEmail, userName, userDate, userSize, userSelectedHour ,userSelectedPeriod } = req.body;
+    const { reservationId, userEmail, userName, userDate, userSize, userTime ,userPeriod } = req.body;
     console.log(req.body);
     console.log(reservationId);
     console.log(userEmail);
@@ -88,11 +88,11 @@ export const confirmingEmail = async (req, res) => {
             from: "opulenzaverve@gmail.com", // Sender's email address
             to: `${userEmail}`, // Recipient's email address
             subject: "Reservation Confirmation", // Email subject
-            text: `Dear ${userName},\n\nWe are thrilled to confirm your table reservation at Opulenza Verve for ${userDate} at ${userSelectedHour + userSelectedPeriod }. We are waiting to welcome you for a delightful dining experience.\n\nReservation Details:\n- Date: ${userDate}\n- Time: ${userSelectedHour + userSelectedPeriod }\n- Guests: ${userSize}\n- Reservation Name: ${userName}\n\nIf you have any special requests or need to make changes, feel free to contact us at opulenzaverve@gmail.com.\nWe're excited to host you and make your visit unforgettable.\n\nBest regards,\nOpulenza Verve`,
+            text: `Dear ${userName},\n\nWe are thrilled to confirm your table reservation at Opulenza Verve for ${userDate} at ${userTime + userPeriod }. We are waiting to welcome you for a delightful dining experience.\n\nReservation Details:\n- Date: ${userDate}\n- Time: ${userTime + userPeriod }\n- Guests: ${userSize}\n- Reservation Name: ${userName}\n\nIf you have any special requests or need to make changes, feel free to contact us at opulenzaverve@gmail.com.\nWe're excited to host you and make your visit unforgettable.\n\nBest regards,\nOpulenza Verve`,
             attachments: [
                 {
                     filename: 'ReservationConfirmation.txt',
-                    content: `Dear ${userName},\n\nWe are thrilled to confirm your table reservation at Opulenza Verve for ${userDate} at ${userSelectedHour + userSelectedPeriod}. We are waiting to welcome you for a delightful dining experience.\n\nReservation Details:\n- Date: ${userDate}\n- Time: ${userSelectedHour + userSelectedPeriod}\n- Guests: ${userSize}\n- Reservation Name: ${userName}\n\nIf you have any special requests or need to make changes, feel free to contact us at opulenzaverve@gmail.com.\nWe're excited to host you and make your visit unforgettable.\n\nBest regards,\nOpulenza Verve`,
+                    content: `Dear ${userName},\n\nWe are thrilled to confirm your table reservation at Opulenza Verve for ${userDate} at ${userTime + userPeriod}. We are waiting to welcome you for a delightful dining experience.\n\nReservation Details:\n- Date: ${userDate}\n- Time: ${userTime + userPeriod}\n- Guests: ${userSize}\n- Reservation Name: ${userName}\n\nIf you have any special requests or need to make changes, feel free to contact us at opulenzaverve@gmail.com.\nWe're excited to host you and make your visit unforgettable.\n\nBest regards,\nOpulenza Verve`,
                 },
             ],
         };
@@ -114,7 +114,7 @@ export const confirmingEmail = async (req, res) => {
 };
 
 export const deletingEmail = async (req, res) => {
-    const { reservationId, userEmail, userName, userDate, userSize, userSelectedHour , userSelectedPeriod} = req.body;
+    const { reservationId, userEmail, userName, userDate, userSize, userTime , userPeriod} = req.body;
     console.log(req.body);
     console.log(reservationId);
     console.log(userEmail);
@@ -143,7 +143,7 @@ export const deletingEmail = async (req, res) => {
             from: "opulenzaverve@gmail.com", // Sender's email address
             to: `${userEmail}`, // Recipient's email address
             subject: "Reservation Cancelled", // Email subject
-            text: `Dear ${userName},\n\nWe regret to inform you that your reservation at Opulenza Verve for ${userDate} at ${userSelectedHour + userSelectedPeriod} has been canceled.\n\nReservation Details:\n- Date: ${userDate}\n- Time: ${userSelectedHour + userSelectedPeriod}\n- Guests: ${userSize}\n- Reservation Name: ${userName}\n\nIf you need assistance or wish to reschedule, please contact us at opulenzaverve@gmail.com.\n\nWe hope to welcome you back in the future.\n\nBest regards,\n Opulenza Verve`, // Email text
+            text: `Dear ${userName},\n\nWe regret to inform you that your reservation at Opulenza Verve for ${userDate} at ${userTime + userPeriod} has been canceled.\n\nReservation Details:\n- Date: ${userDate}\n- Time: ${userTime + userPeriod}\n- Guests: ${userSize}\n- Reservation Name: ${userName}\n\nIf you need assistance or wish to reschedule, please contact us at opulenzaverve@gmail.com.\n\nWe hope to welcome you back in the future.\n\nBest regards,\n Opulenza Verve`, // Email text
         };
 
         // Send the email
